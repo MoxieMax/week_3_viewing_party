a1fa63fc65aacdbc943e626b840357284bd43f57
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-13 15:36:43 -0700

    Complete user story 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,18 @@ class UsersController <ApplicationController
   end 
 
   def create 
-      user = User.create(user_params)
-      if user.save
-          redirect_to user_path(user)
-      else  
-          flash[:error] = user.errors.full_messages.to_sentence
-          redirect_to register_path
-      end 
+    user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else  
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to register_path
+    end 
   end 
 
   private 
 
   def user_params 
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end 
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'password123'
+    fill_in :user_password_confirmation, with:'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
@@ -13,16 +15,42 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: 'password111')
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with:'password123'
+    fill_in :user_password_confirmation, with:'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+  
+  it "does not create a user if passwords don't match" do 
+    visit register_path
     
+    fill_in :user_name, with: 'User New'
+    fill_in :user_email, with:'u2@example.com'
+    fill_in :user_password, with:'password123'
+    fill_in :user_password_confirmation, with:'password111'
+    click_button 'Create New User'
+    
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+  
+  it 'does not create a user if password is blank' do 
+    visit register_path
+  
+    fill_in :user_name, with: 'User Two'
+    fill_in :user_email, with:'unique@example.com'
+  
+    click_button 'Create New User'
+    
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password can't be blank")
   end
 end
