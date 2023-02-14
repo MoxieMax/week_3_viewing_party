d4a59b2fae75077840976fea2f142ad54718ca18
Max MacGillivray <mary.macgillivray@gmail.com>
2023-02-13 22:02:18 -0700

    Started working on US 3 before I had to leave for datcare pickup

---
+++ spec/features/login_form_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  it "can log in with valid credentials" do
+    user = User.create(name: "funbucket13", password: "test")
+
+    visit root_path
+
+    click_on "I already have an account"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :name, with: user.name
+    fill_in :password, with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to have_content("Welcome, #{user.name}")
+  end
+end
